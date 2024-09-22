f5e91fdb409571e5c1309f92e421d7532e79721c
Allen Odoom <allencvb@gmail.com>
2024-09-22 01:49:40 -0400

    Fixing scrollIntroView

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,39 +2,39 @@ import React from 'react';
 import Typewriter from './Typewriter';
 import styles from './Hero.module.scss';
 
-let Scrollchor;
-try {
-  Scrollchor = require('react-scrollchor').default;
-} catch (error) {
-  console.warn('Scrollchor could not be loaded. Using a regular anchor tag instead.');
-  Scrollchor = ({ to, children }) => <a href={to}>{children}</a>;
-}
+const Hero = () => {
+  const scrollToSection = (sectionId) => {
+    const section = document.getElementById(sectionId);
+    if (section) {
+      section.scrollIntoView({ behavior: 'smooth' });
+    } else {
+      console.error(`Section with id "${sectionId}" not found`);
+    }
+  };
 
-const Hero = () => (
-  <div className={styles.heroContainer}>
-    <Typewriter
-      className={styles.heroTitle}
-      text="Allen Odoom"
-      minTypeSpeed={100}
-      maxTypeSpeed={150}
-      initDelay={700}
-    />
-    <div className={styles.heroDescription}>
-      I am a college student and Software Engineer. I have a passion for learning, building beautiful user experiences, and giving back to communities.
-
-      Check out my <Scrollchor to="#projects">side-projects</Scrollchor> below.
-
-      <p className={styles.heroMessage}>
-        <em>
-        Can click on project image to see github!
-        </em>
-    </p>
+  return (
+    <div className={styles.heroContainer}>
+      <Typewriter
+        className={styles.heroTitle}
+        text="Allen Odoom"
+        minTypeSpeed={100}
+        maxTypeSpeed={150}
+        initDelay={700}
+      />
+      <div className={styles.heroDescription}>
+        I am a college student and Software Engineer. I have a passion for learning, building beautiful user experiences, and giving back to communities. 
+        Check out my <a onClick={() => scrollToSection('projects')} style={{ cursor: 'pointer', textDecoration: 'underline' }}>side-projects</a> below.
+        <p className={styles.heroMessage}>
+          <em>
+          Can click on project image to see github!
+          </em>
+        </p>
+      </div>
+      <p className={styles.heroNote}>
+        P.S. Try ejecting the page!
+      </p>
     </div>
-    {/* Add the new italic text here */}
-    <p className={styles.heroNote}>
-      P.S. Try ejecting the page!
-    </p>
-  </div>
-);
+  );
+};
 
 export default Hero;
